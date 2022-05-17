c90989154c43e0db3389acbf872534cdee91e055
Willams Junior <juniormodern@hotmail.com>
2022-05-17 20:00:16 -0300

    implementandoHistorico

--- gamagit/src/Home.js
+++ gamagit/src/Home.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 
 function App(props) {
+  const navigate = useNavigate();
   const [ usuario, setUsuario ] = useState('');
   function handlePesquisa() {
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then( Response =>console.log(Response.data) )
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then( response => {
+    const repositories = response.data;
+    const repositoriesName = [];
+    repositories.map((repository)=> repositoriesName.push(repository.name));
+    localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));   
+    navigate('./repositories');
+});
   }
-
 
   return (
     <>
--- gamagit/src/Repositories.js
+++ gamagit/src/Repositories.js
@@ -3,5 +3,5 @@ import React from "react";
 export default function Repositories() {
     return (
         <h1>Repositories</h1>
-    )
+  )
 };
